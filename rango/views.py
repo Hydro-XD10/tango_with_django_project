d849395fa47291141a48b057354e9306d994b1c3
Abdulmalik <3075614A@student.gla.ac.uk>
2026-02-10 18:32:43 +0000

    adding media and statics as well chapter 4 tests done

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy,cupcake!'} ## Construct a dictionary to pass to the template engine as its context.
+    context_dict={'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'} ## Construct a dictionary to pass to the template engine as its context.
 # Note the key boldmessage matches to {{ boldmessage }} in the template!
 
     return render(request,'rango/index.html',context=context_dict)## Return a rendered response to send to the client.
@@ -10,8 +10,13 @@ def index(request):
 # Note that the first parameter is the template we wish to use.
 
 def about(request):
+    
+    return render(request, 'rango/about.html')
+ 
+'''  
     return HttpResponse(
         'Rango says here is the about page.<br />'
         '<a href="/rango/">Index</a>'
     )
+'''
 # Create your views here.
